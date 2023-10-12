6a20358cc8147b6ae68812aa87d7183139c744d3
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-10-12 20:18:06 +0800

    Complete Python Pythagorean Triplet

---
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -0,0 +1,17 @@
+def validate_triplet(triplet):
+    a, b, c = triplet
+    return a**2 + b**2 == c**2
+
+
+def triplets_with_sum(number):
+    list_of_triplets = []
+
+    # a must be less than 1/3 of the number
+    for a in range(1, number // 3):
+        # b must be less than 1/2 of the number but greater than a
+        for b in range(a + 1, number // 2):
+            # since a + b + c = number, c = number - a - b using algebra
+            c = number - a - b
+            if validate_triplet((a, b, c)):
+                list_of_triplets.append([a, b, c])
+    return list_of_triplets
