ad14bf791be2a5b7cb1c4c184b46b633209df0b8
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-10-12 21:21:55 +0800

    Use a generator function

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,12 +1,9 @@
 def triplets_with_sum(number):
-    list_of_triplets = []
+    return [[a, b, c] for a, b, c in create_triplet(number) if a * a + b * b == c * c]
 
-    # a must be less than 1/3 of the number
-    for a in range(1, number // 3):
-        # b must be less than 1/2 of the number but greater than a
-        for b in range(a + 1, number // 2):
-            # since a + b + c = number, c = number - a - b using algebra
+
+def create_triplet(number):
+    for a in range(number // 3 + 1):
+        for b in range(a, (number - a + 1) // 2):
             c = number - a - b
-            if a**2 + b**2 == c**2:
-                list_of_triplets.append([a, b, c])
-    return list_of_triplets
+            yield [a, b, c]
