d066b2091a16ef7528302a3ffd6d07c4818f9915
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-11-23 13:31:40 +0800

    Rename set to elements to avoid confusion

--- python/custom-set/custom_set.py
+++ python/custom-set/custom_set.py
@@ -1,39 +1,39 @@
 class CustomSet:
     def __init__(self, elements: list = []):
-        self.set = set()
+        self.elements = set()
 
         for e in elements:
             self.add(e)
 
     def isempty(self):
-        return len(self.set) == 0
+        return len(self.elements) == 0
 
     def __contains__(self, element):
-        return element in self.set
+        return element in self.elements
 
     def issubset(self, other):
-        for e in self.set:
-            if e not in other.set:
+        for e in self.elements:
+            if e not in other.elements:
                 return False
         return True
 
     def isdisjoint(self, other):
-        for e in self.set:
-            if e in other.set:
+        for e in self.elements:
+            if e in other.elements:
                 return False
         return True
 
     def __eq__(self, other):
-        return self.set == other.set
+        return self.elements == other.elements
 
     def add(self, element):
-        self.set.add(element)
+        self.elements.add(element)
 
     def intersection(self, other):
-        return CustomSet([e for e in self.set if e in other.set])
+        return CustomSet([e for e in self.elements if e in other.elements])
 
     def __sub__(self, other):
-        return CustomSet([e for e in self.set if e not in other.set])
+        return CustomSet([e for e in self.elements if e not in other.elements])
 
     def __add__(self, other):
-        return CustomSet(list(self.set) + list(other.set))
+        return CustomSet(list(self.elements) + list(other.elements))
