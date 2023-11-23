5be3d8e6792f2d05a1225249c2cd893fd22588c5
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-11-23 10:17:40 +0800

    Remove unnecessary code

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -33,18 +33,3 @@ class Hangman:
 
     def get_status(self):
         return self.status
-
-
-game = Hangman("aaa")
-for ch in "bcdefghij":
-    game.guess(ch)
-    print(game.remaining_guesses)
-    print(game.get_status())
-    print(game.get_masked_word())
-game.guess("a")
-# self.assertEqual(game.remaining_guesses, 0)
-# self.assertEqual(game.get_status(), hangman.STATUS_WIN)
-# self.assertEqual(game.get_masked_word(), "aaa")
-print(game.remaining_guesses)
-print(game.get_status())
-print(game.get_masked_word())
