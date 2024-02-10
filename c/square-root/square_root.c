031aad1e9dcab605c7e84b3b2a2bd0190389a491
Euan Jed Tabamo <tabamoeuan@gmail.com>
2024-02-10 14:03:41 +0800

    Square Root - C

---
+++ c/square-root/square_root.c
@@ -0,0 +1,7 @@
+#include "square_root.h"
+#include <math.h>
+
+double square_root(double radicand)
+{
+    return pow(radicand, 0.5);
+}
---
+++ c/square-root/square_root.h
@@ -0,0 +1,6 @@
+#ifndef SQUARE_ROOT_H
+#define SQUARE_ROOT_H
+
+double square_root(double radicand);
+
+#endif
