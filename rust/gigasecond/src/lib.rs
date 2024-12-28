55e11d81c494edad2df89a71bd8130fbd453e4e9
Euan Jed Tabamo <tabamoeuan@gmail.com>
2024-12-28 12:16:14 +0800

    Remove Rust solutions due to track reset for relearning

--- rust/gigasecond/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use time::Duration;
-use time::PrimitiveDateTime as DateTime;
-
-// Returns a DateTime one billion seconds after start.
-pub fn after(start: DateTime) -> DateTime {
-    start.saturating_add(Duration::seconds(1_000_000_000))
-}
