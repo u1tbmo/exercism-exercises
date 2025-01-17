d4d0149bd06303cf4b7189c49c823afc952e6708
Euan Jed Tabamo <tabamoeuan@gmail.com>
2025-01-18 00:26:53 +0800

    Resistor Color Duo - Typescript

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,26 @@
+// A Color type, which is just an element of the COLORS array
+type Color = typeof COLORS[number];
+
+// This function now only accepts a Color
+export const colorCode = (color: Color) => {
+  return COLORS.indexOf(color);
+};
+
+// This function accepts a tuple of colors with at least two Colors
+export function decodedValue(colors: [Color, Color, ...Color[]]) {
+  let result = colors.slice(0, 2).map(colorCode).join("");
+  return parseInt(result);
+}
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+] as const;
